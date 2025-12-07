19d3162995bc8352fbb39f756b3a0f2d4b76d401
Daniel Kazmer <dkazmer@live.ca>
2025-12-06 22:00:47 -0500

    resize

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { Observer } from './observers';
+import { Obsidian } from './observers';
 
-window.o = new Observer(document.body)
+window.mo = new Obsidian.mutation(document.body)
 	.on('added', x => console.log('>> added!', x))
 	.on('removed', y => console.log('>> remved!', y))
 	.on('attr', z => console.log('>> attr!', z));
 
+window.ro = new Obsidian.resize(document.body).on('resize', x => console.log('>> resized!', x));
+
 declare global {
-	var o: Observer;
+	var mo: Obsidian;
+	var ro: Obsidian;
 }
--- src/observers.ts
+++ src/observers.ts
@@ -1,37 +1,21 @@
-export class Observer {
-	#observer: MutationObserver;
-	#notify: Notify = {};
+abstract class Observer<
+	T extends MutationObserver | ResizeObserver | IntersectionObserver,
+	OnKeys extends keyof Notify
+> {
 	#isSuspended = true;
 
+	protected notify: Notify = {};
+	protected observer!: T;
+	public type = '';
+
 	constructor(
-		private target: Node,
+		private target: T extends MutationObserver ? Node : Element,
 		private settings?: MutationObserverInit
 	) {
-		this.#observer = new MutationObserver((list, obs) => {
-			for (const mutation of list) {
-				switch (mutation.type) {
-					case 'childList':
-						{
-							const { addedNodes, removedNodes } = mutation;
-							addedNodes.length && this.#notify.added?.(addedNodes);
-							removedNodes.length && this.#notify.removed?.(removedNodes);
-						}
-						break;
-
-					case 'attributes':
-						{
-							const { target: t, attributeName } = mutation;
-							this.#notify.attr?.({ attribute: attributeName, target: t });
-						}
-						break;
-
-					default:
-						break;
-				}
-			}
+		resolve('Safe to invoke sub class methods').then(() => {
+			this.type = this.observer.constructor.name; // as typeof this.type;
+			// this.restart();
 		});
-
-		this.restart();
 	}
 
 	/**
@@ -43,7 +27,10 @@ export class Observer {
 			return;
 		}
 
-		this.#observer.observe(this.target, { attributes: true, childList: true, subtree: true, ...this.settings });
+		this.observer instanceof MutationObserver
+			? this.observer.observe(this.target, { attributes: true, childList: true, subtree: true, ...this.settings })
+			: this.observer.observe(this.target as Element);
+
 		this.#isSuspended = false;
 	}
 
@@ -56,7 +43,7 @@ export class Observer {
 			return;
 		}
 
-		this.#observer.disconnect();
+		this.observer.disconnect();
 		this.#isSuspended = true;
 	}
 
@@ -64,7 +51,7 @@ export class Observer {
 	 * kill
 	 */
 	public kill() {
-		this.suspend();
+		this.observer.disconnect();
 
 		for (const prop in this) {
 			Object.hasOwn(this, prop) && delete this[prop];
@@ -74,16 +61,110 @@ export class Observer {
 	/**
 	 * on
 	 */
-	public on(name: keyof Notify, fn: Fn) {
-		this.#notify[name] = (e: any) => fn.call(this, e);
+	public on(name: OnKeys, fn: Fn) {
+		this.notify[name] = (e: any) => fn.call(this, e);
 		return this;
 	}
+}
+
+/**
+ * Intuitive wrapper class for the JS observers:
+ * - {@linkcode Obsidian.mutation|mutation} _MutationObserver_
+ * - {@linkcode Obsidian.resize|resize} _ResizeObserver_
+ * - {@linkcode Obsidian.intersection|intersection} _IntersectionObserver_
+ * @summary Created to encourage greater use of these high-value JS utilities,
+ * as they're vastly underused and unknown, largely for their complex implementation.
+ * @author Daniel B. Kazmer
+ * @version 1.0.0
+ */
+export namespace Obsidian {
+	/**
+	 * Intuitive wrapper class for `ResizeObserver`. Controls:
+	 * - {@linkcode Observer.suspend|.suspend()}
+	 * - {@linkcode Observer.restart|.restart()}
+	 * - {@linkcode Observer.kill|.kill()}
+	 * @author Daniel B. Kazmer
+	 * @version 1.0.0
+	 * @example
+	 * const obsidR = new Obsidian.resize(element)
+	 *    .on('resize', myCallback);
+	 */
+	export class resize extends Observer<ResizeObserver, Extract<keyof Notify, 'resize'>> {
+		constructor(target: Element) {
+			super(target);
+
+			this.observer = new ResizeObserver(entries => {
+				for (const entry of entries) {
+					if (entry.contentBoxSize) {
+						// const contentBoxSize = entry.contentBoxSize[0];
+						console.log('>> contentBoxSize', entry.contentBoxSize);
+					} else {
+						console.log('>> borderBoxSize', entry.borderBoxSize);
+					}
+					console.log('>> resized?', entry);
+					this.notify.resize?.(entry);
+				}
+			});
+
+			this.restart();
+		}
+	}
+
+	/**
+	 * Intuitive wrapper class for `MutationObserver`. Controls:
+	 * - {@linkcode Observer.suspend|.suspend()}
+	 * - {@linkcode Observer.restart|.restart()}
+	 * - {@linkcode Observer.kill|.kill()}
+	 * @author Daniel B. Kazmer
+	 * @version 1.0.0
+	 * @example
+	 * const obsidM = new Obsidian.mutation(scopeElement)
+	 *    .on('added', myCallbackAdd)
+	 *    .on('removed', myCallbackRmv);
+	 */
+	export class mutation extends Observer<MutationObserver, keyof Omit<Notify, 'resize'>> {
+		constructor(target: Node, settings?: MutationObserverInit) {
+			super(target, settings);
+
+			this.observer = new MutationObserver((list, obs) => {
+				for (const mutation of list) {
+					switch (mutation.type) {
+						// biome-ignore format: compact
+						case 'childList': {
+							const { addedNodes, removedNodes } = mutation;
+							addedNodes.length && this.notify.added?.(addedNodes);
+							removedNodes.length && this.notify.removed?.(removedNodes);
+						} break;
+
+						// biome-ignore format: compact
+						case 'attributes': {
+							const { target: t, attributeName } = mutation;
+							this.notify.attr?.({ attribute: attributeName, target: t });
+						}
+					}
+				}
+			});
+
+			this.restart();
+		}
+	}
+}
+
+/**
+ * Prevents sub class prop assignments before their "registration" proper.
+ * Seems to take precedence over a time-out.
+ * @returns a promise
+ */
+export function resolve<T = string>(msg: T) {
+	return new Promise<T>(res => res(msg));
 }
 
 interface Notify<T = void> {
 	attr?: Fn<T, { attribute: string | null; target: Node }>;
 	added?: Fn<T, NodeList>;
 	removed?: Fn<T, NodeList>;
+	resize?: Fn<T, ResizeObserverEntry>;
 }
 
 type Fn<T = void, U = any> = (...args: U[]) => T;
+export type Obsidian = InstanceType<(typeof Obsidian)['mutation' | 'resize']>;
